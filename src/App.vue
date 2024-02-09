3a15872863179a9d82b5afdaa64d897f87f8b321
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2024-02-09 09:50:06 +0100

    reg- done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Header/>
-  <AppNav />
+  <AppNav/>
 </template>
 
 <script setup>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,33 @@
 <template>
+  <form class="position-absolute top-50 start-50 translate-middle" style="text-align: center;">
+    <div>
+      <label for="nameInput" class="form-label" style="font-weight: bolder; color: orange;">Teljes név</label>
+      <input type="name" class="form-control" id="nameInput">
+    </div>
+    <div>
+      <label for="emailInput" class="form-label" style="color: orange;">Email cím</label>
+      <input type="email" class="form-control" id="emailInput">
+    </div>
+    <div>
+      <label for="inputPassword" class="form-label" style="font-weight: bolder; color: orange;">Jelszó</label>
+      <input type="password" id="inputPassword" class="form-control">
+    </div>
+    <div>
+      <label for="inputPasswordAgain" class="form-label" style="color: orange;">Jelszó újra</label>
+      <input type="password" id="inputPasswordAgain" class="form-control">
+      <div id="passwordHelpBlock" class="form-text">
+        <p style="font-size: 0.7rem;">
+          FONTOS! A jelszavad legyen legalább 8 karakter.
+        </p>
+      </div>
+    </div>
     <div>
 
     </div>
+    <div class="mb-3 d-flex justify-content-center">
+      <button type="submit" class="btn btn-outline-secondary">Regisztráció</button>
+    </div>
+  </form>
 </template>
 
 <script setup>
@@ -9,5 +35,8 @@
 </script>
 
 <style scoped>
+.btn {
+  color: orange;
 
+}
 </style>
