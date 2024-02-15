44765c67a85531334f0ede0ea7f23baaa70012c0
NMTHFTR <benedekosze@gmail.com>
2024-02-15 11:18:10 +0100

    asd

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,16 @@
 <template>
+<h1>Luxury Cars Hungary</h1>
+A tervünk az, hogy egy egyszerű és letisztult weboldalt alkossunk, ami megálja a helyét az osztálytársaink és évfolyamtársai weboldalai mellett is. Szeretnénk, hogy a weboldal használatának élménye legyen könnyű és magától érthető. Szerény véleményünk szerintünk lenne jelentős igény különböző szuper kategóriás autók kibérlésére. Közlekedés szerves része napjainknak, miért is ne lehetne, hogy azokat az utakat valami igazán különleges járműben tegyük meg.
 
+A rendelés folyamat:
+Közel 30 autó áll rendelkezésre, amely csak arra vár, hogy kipróbálja őket. Válassza ki a kiválasztott autót, jelölje ki az időtartamot mikor szeretné az autót használni. És mi felvesszük a kapcsolatot Önnel email formájában.
+
+Készítőkről:
+Rómeó:
+Nemzetközi sztár, vagyonőr, üzletember, etikus hacker, előadóművész, értelmiségi, gróf, koronaherceg, civil politikai elemző, testépítő, író, BRFK által megfigyelt, lehallgatott, követett személy, nemzetbiztonsági kockázati tényező, állandó stratégiai gócpont, a Karinthy gyűrű várományosa, istencsapás, donor és békaember
+
+Benedek:
+Fiatal senki a kölyköknek szár, akiért nem kár, aki megígérté, hogy a ház előtt megvár.
 </template>
 
 <script setup>
