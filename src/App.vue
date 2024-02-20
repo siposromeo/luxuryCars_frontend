992143cc862cb67a55ec405d8a54a80cc7698a2c
NMTHFTR <benedekosze@gmail.com>
2024-02-20 10:15:28 +0100

    csita footer jo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Header></Header>
   <AppNav/>
+  <Footer></Footer>
 </template>
 
 <script setup>
 import { RouterView, RouterLink } from 'vue-router'
 import AppNav from '../src/components/navbar.vue'
 import Header from '../src/components/Header.vue'
+import Footer from '../src/components/Footer.vue'
 </script>
 
 <style>
